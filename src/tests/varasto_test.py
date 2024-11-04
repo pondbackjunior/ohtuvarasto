f6bb9d84da4fa23a77afe10bf0f35f7cf6d97ffb
Ville Lampiselkä <148494501+pondbackjunior@users.noreply.github.com>
2024-11-04 21:45:40 +0200

    varasto branch coverage to 100%

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -38,3 +38,24 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    def test_konstruktori_nollataan(self):
+        self.assertEqual(Varasto(-1).tilavuus, 0.0)
+    
+    def test_saldo_nollataan(self):
+        self.assertEqual(Varasto(1, -1).saldo, 0.0)
+    
+    def test_negatiivinen_maara_palautetaan_None(self):
+        self.assertEqual(self.varasto.lisaa_varastoon(-1), None)
+
+    def test_maara_pienempi_kuin_mahtuu(self):
+        Varasto(2).lisaa_varastoon(3)
+    
+    def test_ota_negatiivinen_maara(self):
+        self.assertAlmostEqual(self.varasto.ota_varastosta(-1), 0)
+    
+    def test_kaikki_mita_voidaan(self):
+        self.varasto.ota_varastosta(100)
+
+    def test_tulostuu(self):
+        print(self.varasto)
